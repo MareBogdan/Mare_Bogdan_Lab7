4b5c6814d678bde94004af6229d69977ecdf4c13
MareBogdan <marebogdan03@gmail.com>
2024-12-06 14:31:41 +0200

    lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,27 @@
-﻿namespace Mare_Bogdan_Lab7
+﻿using System;
+using Mare_Bogdan_Lab7.Data;
+using System.IO;
+
+
+namespace Mare_Bogdan_Lab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
