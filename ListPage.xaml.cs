bd6a927b4f763f3fa446a6e99ffe8bfbbd731220
MareBogdan <marebogdan03@gmail.com>
2024-12-08 14:23:13 +0200

    lab9_final

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -22,4 +22,14 @@ public partial class ListPage : ContentPage
         await Navigation.PopAsync();
     }
 
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+
 }
