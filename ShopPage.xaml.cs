0e3c17209f51705b3120823a51806657d5ade6fb
MareBogdan <marebogdan03@gmail.com>
2024-12-15 15:48:31 +0200

    lab10_perfect

--- ShopPage.xaml.cs
+++ ShopPage.xaml.cs
@@ -1,4 +1,6 @@
 using Mare_Bogdan_Lab7.Models;
+using Microsoft.Maui.Devices.Sensors;
+using Plugin.LocalNotification;
 
 namespace Mare_Bogdan_Lab7;
 
@@ -25,10 +27,28 @@ public partial class ShopPage : ContentPage
         {
             Name = "Magazinul meu preferat" };
         var shoplocation = locations?.FirstOrDefault();
-            /* var shoplocation= new Location(46.7492379, 23.5745597);//pentru
-            Windows Machine */
+        /* var shoplocation= new Location(46.7492379, 23.5745597);//pentru
+        Windows Machine */
 
-            await Map.OpenAsync(shoplocation, options);
+        var myLocation = await Geolocation.GetLocationAsync();
+        /* var myLocation = new Location(46.7731796289, 23.6213886738);
+       //pentru Windows Machine */
+        var distance = myLocation.CalculateDistance(location,
+       DistanceUnits.Kilometers);
+        if (distance < 5)
+        {
+            var request = new NotificationRequest
+            {
+                Title = "Ai de facut cumparaturi in apropiere!",
+                Description = address,
+                Schedule = new NotificationRequestSchedule
+                {
+                    NotifyTime = DateTime.Now.AddSeconds(1)
+                }
+            };
+            LocalNotificationCenter.Current.Show(request);
+        }
+        await Map.OpenAsync(shoplocation, options);
         }
 
 }
